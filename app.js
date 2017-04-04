597788d17be3bd027351d11a8f88e8491b66acee
bchhabra00 <bharatchhabra30@gmail.com>
2017-04-03 23:27:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,88 +1,38 @@
 let getweather=document.getElementById('getweather');
+getweather.addEventListener('click',(e) => {
 
-getweather.addEventListener('click',(e) =>{
-    
-    show(weather);
-    
-    e.preventDefault();
-});
-// let city;
-// let country;
-// let temprature;
-// let linebreak;
-// let box;
-function show(weather){
- let    city=document.getElementById('city').value;
+let city=document.getElementById('city').value;
 let country=document.getElementById('country').value;
-
-let url="api.openweathermap.org/data/2.5/weather?" + city + ','
-    + country + "&APPID=1640f6d668afb60a0a7d29014a9ae5ca";
-    
-
-
-    
-     let   box=document.getElementById('display');    
+let icon=document.createElement('img');
+icon.setAttribute('src','img/codes/');
+let  box=document.getElementById('display');    
 let temprature=document.createElement('span');
-
 temprature.style.display='block';
 box.appendChild(temprature); 
 let description=document.createElement('span');
 description.style.display='block';
-box.appendChild(description);
-let longitude=document.createElement('span');
-longitude.style.display='block';
-let latitude=document.createElement('span')
-latitude.style.display='block';
-let image=document.createElement('img')
-image.style.display='block'
-    temprature.innerHTML="Temprature:" + weather.temprature;
-    description.innerHTML="Description:" + weather.descript;
-    image.src="codes/" + weather.icon + '.png';
-    sendrequest(url);
-    
-}
+box.appendChild(description); 
+box.appendChild(icon);
+let url= "http://api.openweathermap.org/data/2.5/weather?q=" + city + ','
+    + country + "&appid=1640f6d668afb60a0a7d29014a9ae5ca" + "&units=metric";
 
-function sendrequest(url){
-    var xhttp = new XMLHttpRequest();
+
+ let xhttp = new XMLHttpRequest();
+ xhttp.open("GET",url,true);
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
     let data= JSON.parse(xhttp.responseText);
-    let weather={};
-    weather.temprature= k2c(data.main.temp);
-    weather.descript=data.weather.description;
-    weather.icon=data.weather[0].id;
-    }
-    }
-xhttp.open("GET", url, true);
-xhttp.send();
-}
+   temprature.innerHTML="Temprature:" + data.main.temp;
+   description.innerHTML="Description:" + data.weather.main;
+   icon.src="img/codes/" + data.weather[0].id + ".png";
+    };
+   }
+    xhttp.send();  
 
 
 
-function k2c(k){
-    
-    return Math.round(k-273.15);
-}
-
-
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(showPosition);
-   
-}
-
-function showPosition(position){
-    updatebygeo(latitude,longitude)
-}
-
-updatebygeo(lat,long){
-    let    city=document.getElementById('city').value;
-let country=document.getElementById('country').value;
-    sendrequest(url)
-    let url="https://api.opencagedata.com/geocode/v1/json?q=" + city +
-    ',' + country + &key=33d73595805f1fcb35c1d6be39006442;
-    
-}
-
+e.preventDefault()
+})
 
 
 
